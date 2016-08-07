7e2f9e5ea98d835921eb82aa0b7818a55a9b88e2
Paul Musso <paul.musso@orange.com>
2016-08-07 17:45:35 +0200

    chore: rename Index page per Welcome

--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import Root from './components/Layout/Root';
-import Index from './components/Index';
+import Welcome from './components/Welcome';
 import Page from './components/Layout/Page';
 import Contact from './components/Contact';
 
 export default (
   <Route path="/" component={Root}>
-  	<IndexRoute component={Index} />
+  	<IndexRoute component={Welcome} />
   	<Route component={Page}>
   		<Route path="contact" component={Contact} />
   	</Route>
