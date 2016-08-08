85fd19981c60fcc9e6f092b117571a3250a259ff
Paul Musso <paul.musso@orange.com>
2016-08-08 08:01:37 +0200

    feat: add google analytics

--- src/entry.js
+++ src/entry.js
@@ -4,9 +4,16 @@ import { renderToString } from 'react-dom/server'
 import { createHistory, createMemoryHistory } from 'history';
 import { Router, RouterContext, match, browserHistory } from 'react-router';
 import routes from './Routes';
+import ReactGA from 'react-ga';
+
+function logPageView() {
+  ReactGA.set({ page: window.location.pathname });
+  ReactGA.pageview(window.location.pathname);
+}
 
 if (typeof document !== 'undefined') {
-	ReactDOM.render(<Router history={browserHistory} routes={routes} />, document)
+	ReactGA.initialize('UA-82152802-1');
+	ReactDOM.render(<Router history={browserHistory} routes={routes} onUpdate={logPageView} />, document)
 }
 
 export default (locals, callback) => {
