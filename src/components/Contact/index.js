5c8df61040a1ca5086e7200f4b8cd13a045d53ac
Paul Musso <paul.musso@orange.com>
2016-08-28 17:29:08 +0200

    chore: redesign contact page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import s from './styles.css';
 import Helmet from "react-helmet";
+import FontIcon from 'react-toolbox/lib/font_icon';
+
+import s from './styles.css';
 
 export default props => (
-	<div>
+	<div className={s.content}>
 		<Helmet title="Contact" />
 		<h1>Contactez-moi</h1>
 		<div className='Grid Grid--gutters Grid--full large-Grid--fit'>
 			<div className="Grid-cell">
 				<div className={s.section}>
-					<h2>Adresse du cabinet</h2>
+					<FontIcon value='person_pin' />
 					<address>
 						4 avenue des capitouls<br/>
 						31880 La Salvetat Saint Gilles
 					</address>			
 				</div>
 				<div className={s.section}>
-					<h2>Téléphone</h2>
+					<FontIcon value='phone' />				
 					<div>06 33 06 57 38</div>			
 				</div>
 				<div className={s.section}>
-					<h2>Email</h2>
+					<FontIcon value='email' />	
 					<div>
 						<a 
 							href="mailto:audrey.barthelemy@outlook.com?subject=Demande de rendez-vous">
@@ -28,12 +30,14 @@ export default props => (
 						</a>
 					</div>			
 				</div>	
-				<div className={s.section}>
+				<div className={`${s.section} m-t-2`}>
 					<h2>Horaires d'ouverture</h2>
+					<FontIcon value='access_time' />
 					<div>De 9h à 20h (du Lundi au Samedi)</div>			
 				</div>
 				<div className={s.section}>
 					<h2>Tarif des consultations</h2>
+					<FontIcon value='info' />
 					<div>
 						50€ la séance <br/>
 						Une séance dure 45 minutes
